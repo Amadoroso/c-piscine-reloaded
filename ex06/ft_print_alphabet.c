58a48fa22b27b8f122f5a8fa1fadb679f376c269
Andre Benegas Pinho Amador <apinho-a@c1r6s12.42lisboa.com>
2026-04-07 16:44:10 +0100

    ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 16:00:10 by apinho-a          #+#    #+#             */
+/*   Updated: 2026/04/07 16:44:02 by apinho-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char letter)
+{
+	write(1, &letter, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 97;
+	while (letter <= 122)
+	{
+		ft_putchar(letter++);
+	}
+}
+
+/* int	main()
+{
+	ft_print_alphabet();
+	return (0);
+} */
