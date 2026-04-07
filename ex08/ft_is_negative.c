7eb43928fe6ebf8962154f753362935ee427cc5b
André Amador <andreamador@MacBook-Pro.local>
2026-04-07 17:51:55 +0100

    ex08

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apinho-a <apinho-a@.42lisboa.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 17:31:59 by apinho-a          #+#    #+#             */
+/*   Updated: 2026/04/07 17:51:44 by apinho-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char character)
+{
+	write(1, &character, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	char negative;
+	char positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+		ft_putchar(negative);
+	else
+		ft_putchar(positive);
+}
+
+/* int	main()
+{
+	ft_is_negative(-3);
+	return (0);
+} */
