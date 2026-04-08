d4a820f7c6c92eda05cdee87934224ed4aea0c46
André Amador <andreamador@MacBook-Pro.local>
2026-04-08 18:23:43 +0100

    ex15

--- ex15/ft_putstr.c
+++ ex15/ft_putstr.c
@@ -6,7 +6,33 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 14:24:54 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/08 14:24:55 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/08 18:23:32 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char character)
+{
+	write(1, &character, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	char	**ptr;
+	int		index;
+
+	ptr = &str;
+	index = 0;
+	while (*(ptr + index) != 0)
+		ft_putchar(*(*ptr + index++));
+}
+
+/* int	main()
+{
+	char *string;
+
+	string = "ola adeus";
+	ft_putstr(string);
+	return (0);
+} */
