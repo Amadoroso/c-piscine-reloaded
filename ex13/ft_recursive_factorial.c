8eecc6028095b94ef298ff241b6f62427a71d6c1
Andre Benegas Pinho Amador <apinho-a@c1r6s9.42lisboa.com>
2026-04-09 18:56:32 +0100

    corrections

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,24 +6,26 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 11:57:41 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/09 13:32:56 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/09 18:54:03 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+/* #include <stdio.h> */
 
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	if (nb == 0)
+	else if (nb == 0)
 		return (1);
+	else if (nb >= 12)
+		return (0);
 	else
 		return (nb * ft_recursive_factorial(nb - 1));
 }
 
 /* int	main()
 {
-	printf("%d",ft_recursive_factorial(4));
+	printf("%d",ft_recursive_factorial(30));
 	return(0);
 } */
