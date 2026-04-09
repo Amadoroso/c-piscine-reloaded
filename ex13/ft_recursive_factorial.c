ebdfa63d1c9c5e8883a78f5017354eae30dabe20
Andre Benegas Pinho Amador <apinho-a@c1r6s9.42lisboa.com>
2026-04-09 17:16:36 +0100

    ex19

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,11 +6,12 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 11:57:41 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/08 14:00:05 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/09 13:32:56 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
@@ -18,12 +19,11 @@ int	ft_recursive_factorial(int nb)
 	if (nb == 0)
 		return (1);
 	else
-		return (nb * ft_recursive_factorial(nb - 1));		
+		return (nb * ft_recursive_factorial(nb - 1));
 }
 
-
-int	main()
+/* int	main()
 {
 	printf("%d",ft_recursive_factorial(4));
 	return(0);
-}
+} */
--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/09 13:07:13 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/09 17:16:00 by apinho-a         ###   ########.fr       */
+/*   Created: 2026/04/09 13:33:48 by apinho-a          #+#    #+#             */
+/*   Updated: 2026/04/09 17:16:02 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,55 @@ void	ft_putchar(char character)
 	write(1, &character, 1);
 } */
 
-int	main(int argc, char **argv)
+void	ft_putstr(char **argv, int argc)
 {
 	int	arg_count;
 	int	inword_count;
 
-	if (argc == 0)
-		return (0);
 	arg_count = 1;
 	while (arg_count < argc)
 	{
 		inword_count = 0;
-		while (*(*(argv + arg_count) + inword_count) != 0)
+		while ((*(*(argv + arg_count) + inword_count)) != 0)
 			ft_putchar(*(*(argv + arg_count) + inword_count++));
 		ft_putchar('\n');
 		arg_count++;
 	}
+}
+
+int	ft_strcmp(char *str_one, char *str_two)
+{
+	while ((*str_one || *str_two) && *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+	return (*str_one - *str_two);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*holder;
+	int		i;
+
+	if (argc == 0)
+		return (0);
+	else
+	{
+		i = 1;
+		while (i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) < 0)
+			{
+				holder = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = holder;
+				if (i != 1)
+					i--;
+			}
+			else
+				i++;
+		}
+	}
+	ft_putstr(argv, argc);
 }
