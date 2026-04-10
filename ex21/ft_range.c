7062d1c89466ef8958ac4c7887699ddb1ebc6be0
Andre Benegas Pinho Amador <apinho-a@c1r7s10.42lisboa.com>
2026-04-10 14:13:08 +0100

    ex21-22

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,11 +6,44 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 17:39:50 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/09 17:40:24 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/10 13:36:20 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 int	*ft_range(int min, int max)
 {
-	
+	int	range;
+	int	*array;
+	int	index;
+
+	if (min >= max)
+		return (NULL);
+	range = max - min;
+	array = malloc (sizeof(int) * range);
+	index = 0;
+	while (min < max)
+		*(array + index++) = min++;
+	return (array);
 }
+/* #include <stdio.h>
+int	main()
+{
+	int	min;
+	int	max;
+	int index;
+	int	min_holder;
+	
+	min = 50;
+	max = 49;
+	index = 0;
+	min_holder = min;
+	while(min_holder < max)
+	{
+		printf("%d",*(ft_range(min, max) + index++));
+		printf(" ");
+		min_holder++;
+	}
+	return (0);
+} */
